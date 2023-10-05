d1315b95795936ac69cf8b829fa873387a3938cd
Nigel Waswa <waswanigel@gmail.com>
2023-10-05 14:04:00 +0000

    6-size.c trial 3

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,7 +6,8 @@
 #include <stdio.h>
 
 /**
- * main Prints the size of various types on the computer it is compiled and run on
+ * main-Prints the size of various types on
+ * 	the computer it is compiled and run on
  *
  * Return: 0
  */
