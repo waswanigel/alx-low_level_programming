b8573be7271f1d095cbdea0012c29b1dfb929268
Nigel Waswa <waswanigel@gmail.com>
2023-10-05 16:40:08 +0000

    4-puts.c print using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
- * main - prints using puts
- * return: 0
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
-{	puts("\"Programming is like building a multilingual puzzle");
+{	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
