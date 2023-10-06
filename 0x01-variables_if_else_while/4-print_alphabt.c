531543d513be84f317fe286a0734f0f9eaae4858
Nigel Waswa <waswanigel@gmail.com>
2023-10-06 13:41:28 +0000

    4-print_alphabt.c, prints alphabet except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Prints lowercase alphabet except q and e
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'q' && c != 'e')
+		{
+			putchar(c);
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
