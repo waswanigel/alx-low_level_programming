e11fc2402b6fac1aefcfb0154df7cd6bfafde7d1
Nigel Waswa <waswanigel@gmail.com>
2023-10-11 14:55:22 +0000

    1-alphabet.c, print lowercase alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * void - prints alphabet followed by new line
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c='a'; c<='z'; c++)
+		_putchar(c);
+
+	_putchar('\n');
+}
