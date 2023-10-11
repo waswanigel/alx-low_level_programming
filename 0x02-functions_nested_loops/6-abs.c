2010235483a64c6b558d3ad44b4f9612b336a0ec
Nigel Waswa <waswanigel@gmail.com>
2023-10-11 17:16:13 +0000

    6-abs.c - compute integer absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - computes abs value of integer
+ * @i: num whose abs value is computed
+ *
+ * Return: Abs value of integer
+ */
+
+int _abs(int i)
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+		return (-i);
+}
