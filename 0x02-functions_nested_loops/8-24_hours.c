52dfbc7af72455107182ef5ecf10f50cc34281a5
Nigel Waswa <waswanigel@gmail.com>
2023-10-11 18:00:39 +0000

    print 24 hrs

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute in jack's day
+ *
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int hr, min;
+
+	for (hr = 0; hr <= 23; hr++)
+	{
+		for (min = 0; min <= 59; min++)
+		{
+			_putchar((hr / 10) + '0');
+			_putchar((hr % 10) + '0');
+			_putchar(':');
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
