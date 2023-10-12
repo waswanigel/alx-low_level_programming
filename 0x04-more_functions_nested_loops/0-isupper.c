487766fc167ad59838ca11983667ae8f5c9f9773
Nigel Waswa <waswanigel@gmail.com>
2023-10-12 09:03:51 +0000

    0-isupper.c, check if character is uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if character is uppercase
+ * @c: checked character
+ *
+ * Return: 1 if uppercase 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
