5cfc439760129ba9d448877220b6b5faf41d653b
Nigel Waswa <waswanigel@gmail.com>
2023-10-19 21:23:31 +0000

    print integers

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: integer
+ */
+void print_number(int n)
+{
+	unsigned int num1;
+
+	num1 = n;
+	if (n < 0)
+	{
+		_putchar('-');
+		num1 = -n;
+	}
+	if (num1 / 10 != 0)
+	{
+		print_number(num1 / 10);
+	}
+	_putchar(num1 % 10) + '0';
+}
