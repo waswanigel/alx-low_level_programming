f2c83413267aa17b48d5ce7bf1e0aedb6160b433
Nigel Waswa <waswanigel@gmail.com>
2023-10-19 18:50:02 +0000

    compare values of two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: s1[i] - s2[i], 0 on success
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+
+	}
+	return (0);
+
+}
