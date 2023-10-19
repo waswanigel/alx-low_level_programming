a01cf4c39cad4f9eddeca953015ac40be0bf9509
Nigel Waswa <waswanigel@gmail.com>
2023-10-19 20:42:37 +0000

    leet 1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @str: the string
+ * Return: str
+ */
+
+char *leet(char *str)
+{
+	int i, j;
+
+	char ch1[] = "aAeEoOtTlL";
+	char ch2[] = "4433007711";
+
+	for (i = 0; i < str[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (str[i] == ch1[j])
+			{
+				str[i] = ch2[j];
+				break;
+			}
+		}
+
+	}
+	return (str);
+}
+
+
