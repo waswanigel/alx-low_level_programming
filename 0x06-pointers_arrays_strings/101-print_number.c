dfdcb9bed4bcaf7db6e0cf5f736bc2acc49b45c3
Nigel Waswa <waswanigel@gmail.com>
2023-10-19 21:32:20 +0000

    print integers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -12,11 +12,11 @@ void print_number(int n)
 	if (n < 0)
 	{
 		_putchar('-');
-		num1 = -n;
+		num1 = -num1;
 	}
 	if (num1 / 10 != 0)
 	{
 		print_number(num1 / 10);
 	}
-	_putchar(num1 % 10) + '0';
+	_putchar((num1 % 10) + '0');
 }
