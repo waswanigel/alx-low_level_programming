4cea09d5bbd971c6359d4685f80bc203fe2af499
Nigel Waswa <waswanigel@gmail.com>
2023-10-19 21:10:30 +0000

    rotate a string by 13, rot13 encoding

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * rot13 - encoded a string by rotating 13 places
+ * @str: string to be rot13ed
+ * Return: str
+ */
+
+char *rot13(char *str)
+{
+	int i, j;
+
+	char rawStr[] =
+"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+	char strRot13[] =
+"NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; i < str[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (str[i] == rawStr[j])
+			{
+				str[i] = strRot13[j];
+				break;
+			}
+
+		}
+
+	}
+	return (str);
+}
