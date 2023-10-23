4647dd92482acd4435c340b6b45c9e41f3226d9f
Nigel Waswa <waswanigel@gmail.com>
2023-10-23 13:00:50 +0000

    locate a character, return a pointer to its location or null if not found

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in the string
+ * @s: string to search for character
+ * @c: character to be located
+ *
+ * Return: pointer to character c in str s or null if c is not found
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return (NULL);
+}
