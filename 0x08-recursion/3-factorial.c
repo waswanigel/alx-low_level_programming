2f32a25d103f414304dd1c51db2f6092554e88ac
Nigel Waswa <waswanigel@gmail.com>
2023-10-25 19:31:03 +0000

    calculate the factorial of a given number

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * factorial - returns the factorial of a given number
+ * @n: the number
+ * Return: returns the factorial or -1 if (n < 0)
+ */
+
+int factorial(int n);
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n <= 1)
+	{
+		return (1);
+	}
+	return (n * factorial(n - 1));
+}
