3c399e9059f32140c79af17979e3f37635dbc692
Nigel Waswa <waswanigel@gmail.com>
2023-10-27 15:28:28 +0000

    0-whatsmyname.c, trial 2

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,8 +8,10 @@
  *
  * Return: 0 on success
  */
-int main(int __attribute__((unused)) argc, char *argv[])
+int main(int argc, char *argv[])
 {
+	(void)argc;
+
 	printf("%s\n", argv[0]);
 	return (0);
 }
