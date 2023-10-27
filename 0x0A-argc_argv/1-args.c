c1ec4d61cdd6a60a9211c316224ea8784fabc522
Nigel Waswa <waswanigel@gmail.com>
2023-10-27 16:06:12 +0000

    print the number of CL arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints the number of arguments
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
