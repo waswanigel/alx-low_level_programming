6803ce1b3154cb43c6e18fd2e816c4d048a97765
Nigel Waswa <waswanigel@gmail.com>
2023-10-27 15:12:16 +0000

    CL args initial commit with readme, main.h and 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints its name followed by new line
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 on success
+ */
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
