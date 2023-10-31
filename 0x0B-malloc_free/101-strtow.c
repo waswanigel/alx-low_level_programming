c3e6e166ddd6c6219538b33196e0dc1fb239fcfe
Nigel Waswa <waswanigel@gmail.com>
2023-10-31 23:40:51 +0000

    strtow try 1

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,84 @@
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * countWords - counts words in a string
+ * @str: the string
+ * @d: the delimeter
+ * Return: the number of words
+ */
+
+int countWords(char *str, char *d);
+
+int countWords(char *str, char *d)
+{
+	char *portion, *temp;
+	int wordcount;
+
+	if (str == NULL || *str == '\0')
+	{
+		return (NULL);
+	}
+	wordcount = 0;
+
+	temp = strdup(str);
+
+	if (temp == NULL)
+	{
+		return (NULL);
+	}
+	portion = strtok(temp, d);
+
+	while (portion != NULL)
+	{
+		wordcount++;
+
+		portion = strtok(NULL, d);
+	}
+	free(temp);
+	return (wordcount);
+}
+/**
+ * strtow - splits a string to words
+ * @str: the string
+ * Return: the split words, NULL if str is NULL/ empty of func fails
+ */
+
+char **strtow(char *str)
+{
+	char **words, d[] = " ";
+	int wordcount = countWords(str, d);
+	int *portion, index = 0, i;
+
+	if (str == NULL || *str == '\0')
+	{
+		return (NULL);
+	}
+	words = (char **)malloc((wordcount + 1) * sizeof(char *));
+
+	if (words == NULL)
+	{
+		return (NULL);
+	}
+	portion = strtok(str, d);
+	while (portion != NULL)
+	{
+		words[index] = strdup(portion);
+		if (words[index] == NULL)
+		{
+			for (i = 0; i < index; i++)
+			{
+				free(words[i]);
+			}
+			free(words);
+			return (NULL);
+		}
+		index++;
+
+		portion = strtok(NULL, d);
+	}
+	words[wordcount] = NULL;
+
+	return (words);
+}
