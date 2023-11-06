881a2057a67fb9c59084bcefba7de704b6caebb3
Nigel Waswa <waswanigel@gmail.com>
2023-11-06 20:32:19 +0000

    added a header file that defines a macro named PI

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -4,7 +4,7 @@
 /**
  * File: 1-pi.h
  * Auth: Nigel Waswa
- * Description: header file that defines a macro named PI as an abbreviation 
+ * Description: header file that defines a macro named PI as an abbreviation
  *		for the token 3.14159265359
  */
 #define PI 3.14159265359
