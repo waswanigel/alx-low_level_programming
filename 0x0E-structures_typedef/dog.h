801f435758dc414f450908883b5ce91e3a09a27d
Nigel Waswa <waswanigel@gmail.com>
2023-11-06 15:28:54 +0000

    updated dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,6 +18,7 @@ struct dog
 /**
  * dog_t - new name for the type struct dog
  */
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
