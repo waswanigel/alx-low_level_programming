bdc3f067983bb7b2af8cdd2a7599f14cb5eded8a
Nigel Waswa <waswanigel@gmail.com>
2023-11-09 19:30:35 +0000

    variadic first commit

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,33 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - perfoms addition of all its parameters
+ * @n: number of parameters to be passed to the function
+ * @...: undefined number of parameters to be summed
+ *
+ * Return: sum of parameters or 0 if n == 0
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list args;
+	int sum = 0;
+
+	if (n == 0)
+	{
+		return 0;
+	}
+	
+	va_start(args, n);
+
+	for (unsigned int i = 0; i < n; i++)
+	{
+		sum += va_arg(args, int);
+	}
+
+	va_end(args);
+
+	return sum;
+}
+
