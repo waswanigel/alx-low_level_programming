d7c87d8be4c134f64e37d568a78bba7e2bb4c1a3
Nigel Waswa <waswanigel@gmail.com>
2023-11-09 19:49:00 +0000

    1-print_numbers.c - first commit of function that prints numbers passed as its parameters

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints number arguments
+ * @separator: string to be printed between numbers
+ * @n: number of integers passed to the function
+ * @...: elipsis
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list nums;
+
+	va_start(nums, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d\n", va_arg(nums, int));
+
+		if (i != (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+
+	va_end(nums);
+}
