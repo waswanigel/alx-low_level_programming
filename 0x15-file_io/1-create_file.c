d66843bfb8c3a5dde592fb80a50593e68702920c
Nigel Waswa <waswanigel@gmail.com>
2023-11-28 01:29:10 +0000

    task 1: 1-create_file.c - function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 	int trunC = O_CREAT | O_WRONLY | O_TRUNC;
 
 	if (filename == NULL)
-		return -1;
+		return (-1);
 
 	if (text_content != NULL)
 		trunC |= O_APPEND;
